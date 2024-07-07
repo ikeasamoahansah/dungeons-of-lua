1721db8ea64add8fa7ba81997d209b608e64b3f3
Ike <dylanraspino264@gmail.com>
2024-07-07 14:55:55 +0000

    fixed map

--- main.lua
+++ main.lua
@@ -8,11 +8,12 @@ function love.load()
 
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setTitle("SEGETES - The Game")
-    love.window.setFullscreen(true)
+    love.window.setMode(800, 600)
+    -- love.window.setFullscreen(true)
 
     player = {}
-    player.x = 400
-    player.y = 300
+    player.x = 0
+    player.y = 0
     player.speed = 3
     player.sprite_sheet = love.graphics.newImage('assets/sprites/Fumiko.png')
     player.grid = anim8.newGrid(24, 32, player.sprite_sheet:getWidth(), player.sprite_sheet:getHeight())
@@ -22,6 +23,11 @@ function love.load()
     player.animations.down = anim8.newAnimation(player.grid('1-3', 3), 0.2)
     player.animations.left = anim8.newAnimation(player.grid('1-3', 4), 0.2)
     player.animations.right = anim8.newAnimation(player.grid('1-3', 2), 0.2)
+
+    -- player.animations.attack.up = anim8.newAnimation(player.grid('1-5', 5), 0.2)
+    -- player.animations.attack.down = anim8.newAnimation(player.grid('1-5', 7), 0.2)
+    -- player.animations.attack.left = anim8.newAnimation(player.grid('1-5', 8), 0.2)
+    -- player.animations.attack.right = anim8.newAnimation(player.grid('1-5', 6), 0.2)
 
     player.anim = player.animations.down
 
@@ -51,6 +57,10 @@ function love.update(dt)
     if love.keyboard.isDown("escape") then
        love.event.quit()
     end
+
+    -- if love.keyboard.isDown("space") then
+    --     player.anim = player.animations.attack.down
+    -- end
 
     if keyPressed == false then
         player.anim:gotoFrame(2)
@@ -88,8 +98,8 @@ end
 
 function love.draw()
     cam:attach()
-        gameMap:drawLayer(gameMap.layers["Divisions"])
         gameMap:drawLayer(gameMap.layers["Ground"])
+        gameMap:drawLayer(gameMap.layers["Divisions"])
         player.anim:draw(player.sprite_sheet, player.x, player.y, nil, 2, nil, 12, 16)
         -- cam:zoomTo(3.5)
     cam:detach()
