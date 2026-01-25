00078279b6e67649804693141d2e703b077e6173
ikeasamoahansah <dylanraspino264@gmail.com>
2026-01-25 01:25:10 +0000

    vigenette

--- assets/shaders/vignette.glsl
+++ assets/shaders/vignette.glsl
@@ -1,10 +1,11 @@
-extern number radius = 0.9;
+extern vec2 screen_size;
+extern number radius = 0.75;
 extern number softness = 0.45;
 
 vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
     vec4 tex = Texel(texture, texture_coords);
-    vec2 position = (screen_coords.xy / love.graphics.getDimensions()) - vec2(0.5);
+    vec2 position = (screen_coords.xy / screen_size) - vec2(0.5);
     number len = length(position);
-    number vignette = smoothstep(radius, radius-softness, len);
-    return tex * color * vec4(1.0, 1.0, 1.0, vignette);
+    number vignette = 1.0 - smoothstep(radius - softness, radius, len);
+    return tex * color * vec4(vignette, vignette, vignette, 1.0);
 }
